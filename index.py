d287502f0be97c2909d46de67975427a2b6b234d
Denis <carriere.denis@gmail.com>
2017-06-19 12:17:11 -0400

    Test for Python 3

--- index.py
+++ index.py
@@ -10,23 +10,23 @@ def shapely(operation, geometry1, geometry2):
     geometry2 = asShape(json.loads(geometry2))
 
     if operation == 'crosses':
-        print geometry1.crosses(geometry2)
+        print(geometry1.crosses(geometry2))
     elif operation == 'contains':
-        print geometry1.contains(geometry2)
+        print(geometry1.contains(geometry2))
     elif operation == 'within':
-        print geometry1.within(geometry2)
+        print(geometry1.within(geometry2))
     elif operation == 'equals':
-        print geometry1.equals(geometry2)
+        print(geometry1.equals(geometry2))
     elif operation == 'touches':
-        print geometry1.touches(geometry2)
+        print(geometry1.touches(geometry2))
     elif operation == 'disjoint':
-        print geometry1.disjoint(geometry2)
+        print(geometry1.disjoint(geometry2))
     elif operation == 'intersects':
-        print geometry1.intersects(geometry2)
+        print(geometry1.intersects(geometry2))
     elif operation == 'overlaps':
-        print geometry1.overlaps(geometry2)
+        print(geometry1.overlaps(geometry2))
     elif operation == 'covers':
-        print geometry1.covers(geometry2)
+        print(geometry1.covers(geometry2))
 
 if __name__ == '__main__':
     OPERATION = sys.argv[1]
